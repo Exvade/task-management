4bef2411ab9b104d4b89cbc4a10c52e43b2a8b60
Deft Valian Exanova <deftvalian2411@gmail.com>
2024-05-30 13:32:11 +0700

    modified

--- config.php
+++ config.php
@@ -1,20 +1,22 @@
 <?php
 class Database {
-    private $host = "localhost";
-    private $db_name = "task_management";
-    private $username = "root";
-    private $password = "";
-    public $conn;
+  private $host = "localhost";
+  private $db_name = "nama_database";
+  private $username = "username_db";
+  private $password = "password_db";
+  public $conn;
 
-    public function getConnection() {
-        $this->conn = null;
-        try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->exec("set names utf8");
-        } catch(PDOException $exception) {
-            echo "Connection error: " . $exception->getMessage();
-        }
-        return $this->conn;
-    }
+  public function getConnection() {
+      $this->conn = null;
+      try {
+          $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+          $this->conn->exec("set names utf8");
+      } catch (PDOException $exception) {
+          echo "Connection error: " . $exception->getMessage();
+          return null;  // Pastikan untuk menambahkan ini jika ada exception
+      }
+      return $this->conn;  // Pastikan fungsi ini mengembalikan objek PDO
+  }
 }
+
 ?>
