bb0ebe3c481a28ad07c58a994b26d58c9061eda9
Deft Valian Exanova <deftvalian2411@gmail.com>
2024-06-07 22:28:08 +0700

    dashboard done

--- dashboard.php
+++ dashboard.php
@@ -22,6 +22,17 @@ $num = $stmt->rowCount();
 <head>
   <title>Dashboard</title>
   <script src="https://cdn.tailwindcss.com"></script>
+  <style>
+    .no-scrollbar::-webkit-scrollbar {
+    width: 5px;
+}
+.no-scrollbar::-webkit-scrollbar-track {
+  background: #60a5fa;
+}
+.no-scrollbar::-webkit-scrollbar-thumb {
+  background: white;
+}
+  </style>
 </head>
 
 <body class="bg-[url(./assets/gapura.jpg)] bg-no-repeat bg-cover bg-center min-h-screen w-full flex justify-center items-center flex-col">
@@ -29,9 +40,9 @@ $num = $stmt->rowCount();
   <h1 class="px-4 py-2 text-3xl font-bold text-blue-500 bg-white rounded-md">Welcome to Your Task Management System</h1>
   <div class="w-[1000px] mt-1 0">
     <?php
-    echo "<h2 class='mb-4 text-2xl font-semibold bg-white'>Your Tasks</h2>";
+    echo "<h2 class='px-4 py-2 mt-5 mb-4 text-2xl font-semibold text-white bg-black rounded-md bg-opacity-40'>Your Tasks</h2>";
     if ($num > 0) {
-      echo "<ol class='flex flex-col w-full gap-6 py-8 text-white bg-black bg-opacity-40 h-[300px] overflow-y-scroll'>"; 
+      echo "<ol class='flex flex-col w-full gap-6 py-8 text-white bg-black bg-opacity-40 h-[300px] overflow-y-scroll rounded-md no-scrollbar'>"; 
       $counter = 1;
       while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         extract($row);
@@ -53,17 +64,17 @@ $num = $stmt->rowCount();
 
     
 
-  <form class="max-w-md mt-10 bg-white" action="add_task.php" method="post">
-    <h1>Add Task</h1>
+  <form class="max-w-md p-4 mx-auto mt-10 bg-black rounded-md bg-opacity-40" action="add_task.php" method="post">
+    <h1 class="text-2xl font-semibold text-center text-white">Add Task</h1>
     <div class="mb-5">
-      <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-      <input type="text" name="title" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Play Valorant" required />
+      <label for="title" class="block mb-2 text-sm font-medium text-white dark:text-white">Title</label>
+      <input type="text" name="title" id="title" class="bg-gray-50 border border-gray-300 text-gray-900  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Play Valorant" required />
     </div>
     <div class="mb-5">
-      <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-      <input type="text" name="description" id="description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 h-[100px] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-wrap" placeholder="Playing with friends and with 'No Radiant No Sleep' Mentality" required />
+      <label for="description" class="block mb-2 text-sm font-medium text-white dark:text-white">Description</label>
+      <input type="text" name="description" id="description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-wrap" placeholder="Playing with friends and with 'No Radiant No Sleep' Mentality" required />
     </div>
-    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+    <button type="submit" class="px-6 py-3 text-sm font-medium text-center text-white bg-blue-400 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 sm:w-auto">Add</button>
   </form>
 
   </div>
